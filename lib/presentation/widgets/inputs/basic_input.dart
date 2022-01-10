3d8478d3ae34ce2b1bb9b3fba2609fbf5396082c
Galymzhan <galimdty@gmail.com>
2022-01-10 18:50:09 +0600

    jyuy

--- lib/presentation/widgets/inputs/basic_input.dart
+++ lib/presentation/widgets/inputs/basic_input.dart
@@ -20,12 +20,11 @@ class BasicInput extends StatelessWidget {
         validator: validator,
         controller: controller,
         decoration: InputDecoration(
-          hintStyle: ThemeTextStyle.R18white,
+          hintStyle: ThemeTextStyle.R18Linkblue.copyWith(
+              color: ThemeColors.gray.withOpacity(0.5)),
           enabledBorder: const OutlineInputBorder(
               borderRadius: BorderRadius.all(Radius.circular(4)),
               borderSide: BorderSide(width: 1, color: ThemeColors.white)),
-          errorText: 'Полу пустое',
-          errorStyle: ThemeTextStyle.R18black.copyWith(color: ThemeColors.red),
           filled: true,
           fillColor: ThemeColors.white,
           hintText: hintText,
